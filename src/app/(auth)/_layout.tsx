cdb05960ef65b84ad95f2d37e864426eea296631
Taha Sayyed <codetaha786@gmail.com>
2026-03-30 15:09:00 +0530

    feat: improve authentication layout with loading indicator and redirect adjustments

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,15 +1,21 @@
-import { useAuth } from '@clerk/expo'
 import { Redirect, Stack } from 'expo-router'
+import { useAuth } from '@clerk/expo'
+import { ActivityIndicator, View } from 'react-native'
 
 export default function AuthRoutesLayout() {
   const { isSignedIn, isLoaded } = useAuth()
+  console.log("AuthGroup:", { isLoaded, isSignedIn })
 
   if (!isLoaded) {
-    return null
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" />
+      </View>
+    )
   }
 
   if (isSignedIn) {
-    return <Redirect href={'/'} />
+    return <Redirect href="/(tabs)/list" />
   }
 
   return <Stack screenOptions={{headerShown:false}} />
