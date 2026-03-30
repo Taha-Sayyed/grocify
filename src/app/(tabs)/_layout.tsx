53a2f04d24f0191058308eb8d33338d48e965610
Taha Sayyed <codetaha786@gmail.com>
2026-03-30 15:10:09 +0530

    feat: add layout and tab navigation with insights and planner screens

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,58 @@
+import { Redirect, Stack } from 'expo-router'
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
+import { useAuth } from '@clerk/expo'
+import {useColorScheme, ActivityIndicator, View} from "react-native"
+
+
+export default function Layout() {
+  const { isSignedIn, isLoaded } = useAuth()
+  const colorScheme=useColorScheme();
+  const isDark=colorScheme==="dark";
+  const tabintColor=isDark? "hsl(142 70% 54%)":"hsl(147 75% 33%)"
+
+  console.log("TabsAuth:", { isLoaded, isSignedIn })
+
+  if (!isLoaded) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+
+  if (!isSignedIn) {
+    return <Redirect href="/(auth)/sign-in" />
+  }
+
+  return(
+    <NativeTabs tintColor={tabintColor}>
+        <NativeTabs.Trigger name='list'>
+            <NativeTabs.Trigger.Label>List</NativeTabs.Trigger.Label>
+            <NativeTabs.Trigger.Icon
+                sf={{
+                  default: "list.bullet.clipboard",
+                  selected: "list.bullet.clipboard.fill",
+                }}
+                md="list"
+            />
+        </NativeTabs.Trigger>
+
+        <NativeTabs.Trigger name="planner">
+            <NativeTabs.Trigger.Icon
+              sf={{ default: "plus.circle", selected: "plus.circle.fill" }}
+              // md="add"
+              md="add_circle"
+            />
+            <NativeTabs.Trigger.Label>Planner</NativeTabs.Trigger.Label>
+        </NativeTabs.Trigger>
+
+        <NativeTabs.Trigger name="insights">
+            <NativeTabs.Trigger.Icon
+              sf={{ default: "chart.bar", selected: "chart.bar.fill" }}
+              md="analytics"
+            />
+            <NativeTabs.Trigger.Label>Insights</NativeTabs.Trigger.Label>
+        </NativeTabs.Trigger>
+    </NativeTabs>
+  )
+}
