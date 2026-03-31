141762c95af51ece91e7e31b5808b4dcf69ade18
Taha Sayyed <codetaha786@gmail.com>
2026-03-31 17:20:49 +0530

    feat: implement InsightsScreen with user profile, stats, categories, and priority sections

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import ClearCompletedButton from "@/components/insights/ClearCompletedButton";
+import InsightsCategorySection from "@/components/insights/InsightsCategorySection";
+import InsightsPrioritySection from "@/components/insights/InsightsPrioritySection";
+import InsightsStatsSection from "@/components/insights/InsightsStatsSection";
+import UserProfile from "@/components/insights/UserProfile";
+import TabScreenBackground from "@/components/TabScreenBackground";
+import { ScrollView } from "react-native";
 
-const insights = () => {
+const InsightsScreen = () => {
   return (
-    <View>
-      <Text>insights</Text>
-    </View>
-  )
-}
+    <>
+      <ScrollView
+        className="flex-1 bg-background py-4"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 20, gap: 14 }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <TabScreenBackground />
 
-export default insights
+        <UserProfile />
+        <InsightsStatsSection />
+        <InsightsCategorySection />
+        <InsightsPrioritySection />
+        <ClearCompletedButton />
+      </ScrollView>
+
+      {/* <SentryFeedbackButton /> */}
+    </>
+  );
+};
+
+export default InsightsScreen;
